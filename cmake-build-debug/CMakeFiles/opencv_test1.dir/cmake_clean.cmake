1388a8281702b1baabc2713e94aa757def1354b4
zhaoxuyan <526161421@qq.com>
2018-04-17 17:42:05 +0800

    add cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/opencv_test1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_test1.dir/main.cpp.o"
+  "opencv_test1.pdb"
+  "opencv_test1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
